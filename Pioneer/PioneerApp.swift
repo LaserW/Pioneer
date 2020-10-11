128ed1a1c84ddd5634f73272ca45627c30e909de
Dmitry Berezin <dmitry_berezin@mail.ru>
2020-10-11 23:00:43 +0300

    Initial Commit

---
+++ Pioneer/PioneerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PioneerApp.swift
+//  Pioneer
+//
+//  Created by Березин Дмитрий Борисович on 11.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct PioneerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
